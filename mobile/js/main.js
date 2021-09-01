917714d03fa2ecd9044503a636146eb254bc183c
Ab12346Cd <89125099+Ab12346Cd@users.noreply.github.com>
2021-09-01 18:59:24 +0800

    Add files via upload

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -3,6 +3,8 @@ function myFunction() {
   var menu = document.getElementById("image1");
   var submenu = document.getElementById("s_menu");
   var select1 = document.getElementById("select1");
+  var m4_1 = document.getElementById("m4_1");
+  
   if (menu.style.display === "block") {
     menu.style.display = "block";
 	  if(submenu.style.display === "block"){
@@ -11,10 +13,12 @@ function myFunction() {
 	  }else{
 		submenu.style.display = "block";
 		select1.style.display = "none";
+		m4_1.style.marginTop = "143px";
 	  }
   } else {
     menu.style.display = "block";
 	submenu.style.display = "block";
 	select1.style.display = "none";
+	m4_1.style.marginTop = "143px";
   }
 }
